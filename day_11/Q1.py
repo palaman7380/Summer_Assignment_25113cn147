1cb26df5c042d7c187382026d353a629699670ae
palaman7380 <amanpal231006@gmail.com>
2026-06-08 07:46:18 +0530

    Added day_11 program

---
+++ day_11/Q1.py
@@ -0,0 +1,7 @@
+def sum(a,b):
+    
+    return a+b
+
+result = sum(2,7)
+print(result)
+
---
+++ day_11/Q2.py
@@ -0,0 +1,7 @@
+def max(a,b,c):
+    if a>b and a>c:
+        return a
+    elif b>c:
+        return b
+
+print(max(3,5,4))
